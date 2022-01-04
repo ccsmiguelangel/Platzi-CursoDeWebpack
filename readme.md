afc5f138354d5a26ed15dd06589e6473eace403e
ccsmiguelangel <miguelangel1074.gc@gmail.com>
2022-01-04 18:04:06 -0400

    7

--- readme.md
+++ readme.md
@@ -1,1 +1,25 @@
-# curso-webpack
+git init
+npm init -y
+
+src/index.js
+
+### Instalar webpack:
+`npm install webpack webpack-cli -D`
+
+nota: -D para instalar solo en nuestro proyecto
+
+### Ejecutar webpack
+`npx webpack`
+
+### Ejecutar webpack modo desarrollo y modo producción
+Nota el modo desarrollo contiene comentarios mientras que el entorno de producción no lo contiene
+
+`npx webpack --mode development`
+`npx webpack --mode production`
+
+
+### Ejecutar con config
+`npx webpack --mode production --config webpack.config.js`
+
+### Instalar babel
+`npm i babel-loader @babel/core @babel/preset-env @babel/plugin-transform-runtime -D`
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,32 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.js', // entrada archivo de primera lectura
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'), //dirección de salida dist de distribution
+    filename: 'main.js', // build.js
   },
   resolve: {
     extensions: ['.js']
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/, // expresión regular ve si inicia con m o con js
+        exclude: /node_modules/, // no queremos que caiga nuestra aplicación
+        use: {
+          loader: 'babel-loader', // usa el loader de babel
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
+
 }
